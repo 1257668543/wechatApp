3e1d5584cb4b7aa460688a22d2641269e6b6dd3e
ShadowsXie <1084827833@qq.com>
2020-07-25 20:54:13 +0800

    美团小程序主页

--- meituan/pages/homePage/homePage.js
+++ meituan/pages/homePage/homePage.js
@@ -5,7 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    actives:[
+      {image: '/icon/food.png',
+      activeName: '美食',
+      color:'#f6c944'},
+      {image: '/icon/cateyes.png',
+        activeName: '电影/演出',
+        color:'#eb4e27'},
+      {image: '/icon/hotel.png',
+        activeName: '酒店/民宿',
+        color:'#985b3c'},
+      {image: '/icon/fun.png',
+        activeName: '休闲娱乐',
+        color:'#57bd9a'},
+      {image: '/icon/meituanwaimai.png',
+        activeName: '外卖',
+        color:'#f5c343'},
+    ],
+    moreActivies: [
+      {image: '/icon/supermarket.png',
+      title: '超市便利',
+      color:'#f6c944'},
+      {image: '/icon/visit.png',
+      title: '周边游/旅游',
+      color:'#f6c944'},
+      {image: '/icon/car.png',
+      title: '打车',
+      color:'#f6c944'},
+      {image: '/icon/run.png',
+      title: '跑腿代购',
+      color:'#f6c944'},
+      {image: '/icon/bike.png',
+      title: '骑车',
+      color:'#f6c944'},
+      {image: '/icon/train.png',
+      title: '火车票',
+      color:'#f6c944'},
+      {image: '/icon/vegetable.png',
+      title: '蔬菜水果',
+      color:'#f6c944'},
+      {image: '/icon/minsu.png',
+      title: '民宿/公寓',
+      color:'#f6c944'},
+      {image: '/icon/hair.png',
+      title: '丽人/美发',
+      color:'#f6c944'},
+    ],
+    banners: [
+      {img:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=236680447,2092205433&fm=26&gp=0.jpg'},
+      {img:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=279711226,2088997447&fm=26&gp=0.jpg'},
+      {img:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2411819467,2552031770&fm=26&gp=0.jpg'},
+      {img:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1615884197,1066022113&fm=26&gp=0.jpg'},
+      {img:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1986861299,3519515236&fm=26&gp=0.jpg'},
+      {img:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1653847011,3732062058&fm=26&gp=0.jpg'},
+      {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3127462374,1578645209&fm=26&gp=0.jpg'},
+      {img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2340940098,583321155&fm=26&gp=0.jpg'},
+      {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=230566004,3595917731&fm=26&gp=0.jpg'},
+    ],
+    bottomShop:[
+      {image:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1573848844,97076657&fm=26&gp=0.jpg',
+        title:'香酥大鸡腿',
+        price:7.14
+    },
+    {image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3230719735,502661653&fm=26&gp=0.jpg',
+        title:'蜗牛之家',
+        price:7.14
+    }
+    ]
   },
 
   /**
